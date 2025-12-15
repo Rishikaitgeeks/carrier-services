4e52069bc9bd2b4d7acebd09157c7191f29fe1e3
Rishikaitgeeks <rishikanagotri@itgeeks.com>
2025-12-15 18:07:20 +0530

    newnwe

--- app/routes/new.jsx
+++ app/routes/new.jsx
@@ -1,4 +1,10 @@
 
-export default function loader() {
-    return null;
+export async function action({ request }) {
+    const body = await request.json();
+    console.log(JSON.stringify(body, null, 2));
+
+
+    return body;
 }
+
+
