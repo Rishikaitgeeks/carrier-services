5e65650055d6c7545a2622453203babe10c9e714
Rishikaitgeeks <rishikanagotri@itgeeks.com>
2025-12-15 19:10:06 +0530

    add rate2

--- app/routes/api.get-shipping-count.jsx
+++ app/routes/api.get-shipping-count.jsx
@@ -4,7 +4,7 @@ import { authenticate } from "../shopify.server";
 export const loader = async ({ request }) => {
   const { session } = await authenticate.admin(request);
 
-  const { ShopSettings } = await import("../../models/shopSettings.model.js");
+  const { ShopSettings } = await import("../models/shopSettings.model");
 
   const settings = await ShopSettings.findOne({ shop: session.shop });
 
