b613763195ed188f78f19d6b8b7467e0cb278dd8
Rishikaitgeeks <rishikanagotri@itgeeks.com>
2025-12-16 17:04:48 +0530

    nedsjfnkjskdsf

--- app/routes/new.jsx
+++ app/routes/new.jsx
@@ -1,3 +1,18 @@
+export default function loader({ request }) {
+    if (request.method === "OPTIONS") {
+        return new Response(null, {
+            status: 204,
+            headers: {
+                "Access-Control-Allow-Origin": "*",
+                "Access-Control-Allow-Methods": "POST, OPTIONS",
+                "Access-Control-Allow-Headers": "Content-Type, Authorization",
+            },
+
+        })
+
+    }
+}
+
 
 export async function action({ request }) {
     const body = await request.json();
